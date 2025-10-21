c2eb5e86daee0a1340268328fe3db36d684a9f1b
Wavxr <johnveraguilar25@gmail.com>
2025-10-21 20:24:22 +0800

    feat: add other properties to project interface and created project category interface

--- types/projects.ts
+++ types/projects.ts
@@ -1,8 +1,15 @@
+export type ProjectCategory = "swe-ml" | "university"
+
 export interface Project {
   id: string;
+  slug: string;
   name: string;
-  description?: string;
-  image?: string;
+  description: string;
+  category: ProjectCategory;
+  tags: string[];
+  thumbnail: string;
+  imageFolder: string;
+  imageCount: number;
   link?: string;
   github?: string;
 }
