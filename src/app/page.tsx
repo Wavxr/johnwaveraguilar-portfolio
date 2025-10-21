108f458ba8ee4450679a5ae48859d0db0db0c7bf
Wavxr <johnveraguilar25@gmail.com>
2025-10-21 20:29:17 +0800

    feat: completed home page for portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+"use client"
+
+import Hero from "@/components/sections/hero"
+import Skills from "@/components/sections/skills"
+import { Projects } from "@/components/sections/projects"
+import Certifications from "@/components/sections/certifications"
+import ContactForm from "@/components/sections/contact"
+import Footer from "@/components/layout/footer"
+import NavBar from "@/components/layout/NavBar"
+import { homeNavLinks } from "@/constants/navlinks"
+
 export default function Home() {
-  return (
-    <>
-      <h1>Welcome to the Home Page</h1>
-    </>
-  );
+	return (
+		<>
+			<NavBar navLinks={homeNavLinks} />
+			<Hero />
+			<Skills />
+			<Projects />
+			<Certifications />
+			<ContactForm />
+			<Footer />
+		</>
+	)
 }
